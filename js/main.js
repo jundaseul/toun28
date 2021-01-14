63edf919a9b478b3247278c7e5f37539313b5020
jundaseul <jjdaseul_@naver.com>
2021-01-14 16:24:57 +0900

    수정(1/14)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 $(function () { /////// jQB ///////////////////////
     console.log("로딩완료!");
 
+    /*마우스 커서*/
+    $("body").mousemove(function (e) {
+        var posx = e.pageX - 15;
+        var posy = e.pageY - 15;
+        //console.log("x축:"+posx+"\ny축:"+posy);
+
+        $("#cursor").css({
+            top: posy + "px",
+            left: posx + "px"
+        });
+
+
+    }); //////////////// mousemove ///////////////
+
+
     /*풀페이지*/
     $("#fullpage").fullpage({
         autoScrolling: true,
         scrollHorizontally: true,
-        menu: '#menu',
-        lockAnchors: false,
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+//        menu: '#menu',
+//        lockAnchors: false,
+//        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
         navigation: true,
         navigationPosition: 'right',
     });
@@ -15,5 +30,12 @@ $(function () { /////// jQB ///////////////////////
     /*애니메이션 플러그인*/
     new WOW().init();
     
+    
+    /*로고 클릭시 맨 위 이동*/
+    $('.logo').click(function () {
+        $.fn.fullpage.moveTo(1);
+    }); ///////////////// click ////////////////////////////
+    
+
 }); ////////// jQB ///////////////////////////////
 /////////////////////////////////////////////////
