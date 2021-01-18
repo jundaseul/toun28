c85ed603b8bc575ca1e8ca6447638c7a9828c19e
jundaseul <jjdaseul_@naver.com>
2021-01-18 17:02:52 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -16,26 +16,43 @@ $(function () { /////// jQB ///////////////////////
     }); //////////////// mousemove ///////////////
 
 
+    /*a링크 마우스오버시 커서 확대*/
+    $("a").hover(
+        function () {
+            $("#cursor").addClass("trC");
+        },
+        function () {
+            $("#cursor").removeClass("trC");
+        });
+
+    
+
     /*풀페이지*/
     $("#fullpage").fullpage({
         autoScrolling: true,
         scrollHorizontally: true,
-//        menu: '#menu',
-//        lockAnchors: false,
-//        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        navigation: true,
+        //        menu: '#menu',
+        //        lockAnchors: false,
+        //        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        navigation: false,
         navigationPosition: 'right',
+        responsiveWidth: 1025,
     });
 
     /*애니메이션 플러그인*/
     new WOW().init();
-    
-    
+
+
     /*로고 클릭시 맨 위 이동*/
     $('.logo').click(function () {
         $.fn.fullpage.moveTo(1);
     }); ///////////////// click ////////////////////////////
-    
+
+
+    /*1024px부터 'fp-auto-height' 넣기*/
+    if ($(window).width() < 1025) {
+        $(".section").addClass("fp-auto-height");
+    }
 
 }); ////////// jQB ///////////////////////////////
 /////////////////////////////////////////////////
