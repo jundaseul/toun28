914475c0797c9e17ffdd8ac672a2d243bf1e8fc9
jundaseul <jjdaseul_@naver.com>
2021-01-20 18:43:14 +0900

    수정(1/20)

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(function () { /////// jQB ///////////////////////
             $("#cursor").removeClass("curfill");
         });
 
-    
+
 
     /*풀페이지*/
     $("#fullpage").fullpage({
@@ -36,10 +36,54 @@ $(function () { /////// jQB ///////////////////////
         navigation: false,
         navigationPosition: 'right',
         responsiveWidth: 1025,
+        'afterLoad': function (anchorLink, index) {
+            /*2페이지 텍스트*/
+            if (index == 2) {
+                //console.log("도착?");
+                $(".cont1_txt_ani").stop().animate({
+                    top: "0",
+                    opacity: 1
+                }, 900)
+            }
+            if (index !== 2) { // 해당영역아닐때 다시 밖으로빼놓기(원상복귀)
+                $(".cont1_txt_ani").stop().animate({
+                    top: "5%",
+                    opacity: 0
+                }, 300);
+            }
+
+            /*상품리스트*/
+            if (index == 3) {
+                //console.log("도착?");
+                $(".prod_list").stop().animate({
+                    top: "53%",
+                    opacity: 1
+                }, 700)
+            }
+            if (index !== 3) { // 해당영역아닐때 다시 밖으로빼놓기(원상복귀)
+                $(".prod_list").stop().animate({
+                    top: "150%",
+                }, 300);
+            }
+
+            /*마지막 섹션*/
+            if (index == 4) {
+                //console.log("도착?");
+                $(".cont1_txt_ani.cont2_txt").stop().animate({
+                    top: "0",
+                    opacity: 1
+                }, 900)
+            }
+            if (index !== 4) { // 해당영역아닐때 다시 밖으로빼놓기(원상복귀)
+                $(".cont1_txt_ani.cont2_txt").stop().animate({
+                    top: "5%",
+                    opacity: 0
+                }, 300);
+            }
+
+        }
     });
 
-    /*애니메이션 플러그인*/
-    new WOW().init();
 
 
     /*로고 클릭시 맨 위 이동*/
@@ -52,6 +96,36 @@ $(function () { /////// jQB ///////////////////////
     if ($(window).width() < 1025) {
         $(".section").addClass("fp-auto-height");
     }
+
+
+
+    /*menu버튼 클릭시 메뉴박스 나타나기 - 아이패드 사이즈*/
+    $(".mob_menuBtn").click(function () {
+
+        if ($(".mob_menuBtn").hasClass("on")) {
+            $(this).removeClass("on").find("a").html("Close");
+            $("#bgBox").fadeIn(300);
+            $(".menu_box").animate({
+                left: "0"
+            },800);
+            $("body").css({
+                overflowY:"hidden"
+            });
+        } else {
+            $(this).addClass("on").find("a").html("Menu");
+            $("#bgBox").fadeOut(300);
+            $(".menu_box").animate({
+                left: "-100%"
+            },500);
+            $("body").css({
+                overflowY:"Scroll"
+            });
+        }
+        
+    });
+
+
+
 
 }); ////////// jQB ///////////////////////////////
 /////////////////////////////////////////////////
